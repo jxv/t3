ed7bcb0dbe29dd70e4c69ecd585f629ccc9e4a15
jvargas <jvargas@cj.com>
2016-03-12 16:38:11 -0800

    dummy post to create

--- Handler/Create.hs
+++ Handler/Create.hs
@@ -1,9 +1,15 @@
 module Handler.Create where
 
 import Import
+import T3.Server
 
 getCreateR :: Handler Html
 getCreateR = error "Not yet implemented: getCreateR"
 
-postCreateR :: Handler Html
-postCreateR = error "Not yet implemented: postCreateR"
+postCreateR :: Handler Value
+postCreateR = do
+  userId <- liftIO genUserId
+  userKey <- liftIO genUserKey
+  let creds = object [ "userId" .= userId, "userKey" .= userKey ]
+  liftIO $ print creds
+  returnJson creds
