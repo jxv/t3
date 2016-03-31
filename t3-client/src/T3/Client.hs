683a524770382ca4f522e0f38da13cedf4baabf9
jvargas <jvargas@cj.com>
2016-03-31 09:40:59 -0700

    improve re-exports

--- t3-client/src/T3/Client.hs
+++ t3-client/src/T3/Client.hs
@@ -1,13 +1,45 @@
 module T3.Client
-  ( module T3.Server
-  , module T3.Match
-  , module T3.Random
-  , module T3.Playback
-  , module T3.Game
+  ( XO(..)
+  , Loc(..)
+  , Action(..)
+  , Result(..)
+  , Board
+  , yinYang
+  , emptyBoard
+  , boardMap
+  , boardList
+  , boardSize
+  , insertXO
+  , inside
+  , valid
+  , result
+  , Win(..)
+  , Lose(..)
+  , run
+  , UserCreds(..)
+  , StartRequest(..)
+  , PlayRequest(..)
+  , GameState(..)
+  , Users(..)
+  , StartResponse(..)
+  , PlayResponse(..)
+  , UserName(..)
+  , UserKey(..)
+  , RegisterRequest(..)
+  , RegisterResponse(..)
+  , Users(..)
+  , MatchId(..)
+  , MatchToken(..)
+  , Step(..)
+  , MatchInfo(..)
+  , Final(..)
+  , Result(..)
+  , Playback(..)
+  , randomLoc
   ) where
 
-import T3.Server hiding (GameLogger, Server, forkServer, genBase64, genMatchToken, genMatchId, genUserName, genUserKey, authenticate, authorize, toGameState)
-import T3.Match hiding (StartCallback, Callback, UserInit, runMatch)
+import T3.Server
+import T3.Match
 import T3.Game
 import T3.Random
-import T3.Playback hiding (writePlayback)
+import T3.Playback
