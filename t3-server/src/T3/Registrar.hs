f0ac9a725167487904d9d72c7e651e63d5d438f6
jvargas <jvargas@cj.com>
2016-08-05 06:32:59 -0700

    restart server

--- t3-server/src/T3/Registrar.hs
+++
@@ -1,9 +0,0 @@
-module T3.Registrar
-  ( Registrar(..)
-  ) where
-
-import T3.Server (UserCreds, UserKey, UserName)
-
-class Monad m => Registrar m where
-  register :: UserName -> UserKey -> m ()
-  authenticate :: UserCreds -> m ()
