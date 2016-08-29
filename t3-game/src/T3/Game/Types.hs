454b29b1069450c105f614f8c02bb8eef4264fa7
jvargas <jvargas@cj.com>
2016-08-28 17:16:15 -0700

    t3-game: remove aeson

--- t3-game/src/T3/Game/Types.hs
+++ t3-game/src/T3/Game/Types.hs
@@ -7,7 +7,6 @@ module T3.Game.Types
   ) where
 
 import Control.Monad (mzero)
-import Data.Aeson (ToJSON(..), FromJSON(..), Value(..))
 import Data.Text (Text)
 
 import T3.Core (Board)
@@ -30,19 +29,3 @@ data Final
   | LossByDQ
   | Tied
   deriving (Show, Eq)
-
-instance FromJSON Final where
-  parseJSON (String "Won") = pure Won
-  parseJSON (String "WonByDQ") = pure WonByDQ
-  parseJSON (String "Loss") = pure Loss
-  parseJSON (String "LossByDQ") = pure LossByDQ
-  parseJSON (String "Tied") = pure Tied
-  parseJSON _ = mzero
-
-instance ToJSON Final where
-  toJSON f = String $ case f of
-    Won -> "Won"
-    WonByDQ -> "WonByDQ"
-    Loss -> "Loss"
-    LossByDQ -> "LossByDQ"
-    Tied -> "Tied"
