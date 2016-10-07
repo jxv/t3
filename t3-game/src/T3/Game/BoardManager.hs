64d643ecb26ef6cbe9ea09b649302e4953d92f9d
jxv <joevargas92@gmail.com>
2016-10-07 00:37:09 -0700

    start complicating the game

--- t3-game/src/T3/Game/BoardManager.hs
+++ t3-game/src/T3/Game/BoardManager.hs
@@ -1,11 +1,14 @@
 module T3.Game.BoardManager
   ( BoardManager(..)
   , isOpenLoc'
+  , isOpenLoc_
   , insertAtLoc'
+  , insertAtLoc_
   , getResult'
+  , getResult_
   ) where
 
-import T3.Core (Loc, XO, Result, valid, insertXO, result)
+import T3.Core (Loc, XO, Result, valid, insertXO, result, Board)
 import T3.Game.HasBoard (HasBoard(getBoard, putBoard))
 
 class Monad m => BoardManager m where
@@ -18,12 +21,21 @@ isOpenLoc' loc = do
   board <- getBoard
   return $ valid loc board
 
+isOpenLoc_ :: Loc -> Board -> Bool
+isOpenLoc_ = valid
+
 insertAtLoc' :: HasBoard m => Loc -> XO -> m ()
 insertAtLoc' loc xo = do
   board <- getBoard
   putBoard $ insertXO loc xo board
 
+insertAtLoc_ :: Loc -> XO -> Board -> Board
+insertAtLoc_ = insertXO
+
 getResult' :: HasBoard m => m Result
 getResult' = do
   board <- getBoard
   return $ result board
+
+getResult_ :: Board -> Result
+getResult_ = result
--- t3-server/src/T3/Server/Game.hs
+++ t3-server/src/T3/Server/Game.hs
@@ -5,12 +5,18 @@ module T3.Server.Game
   , main
   ) where
 
+import Control.Concurrent.Chan (readChan)
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.Reader (ReaderT(runReaderT), MonadReader(..), asks)
+import Control.Monad.State (StateT(..), evalStateT, MonadState(..))
 
+import T3.Core (emptyBoard, Board, XO(..), Loc, Result)
+import T3.Game.Main (main)
+import T3.Game.Play (Play(..), play')
+import T3.Game.Control (Control(..))
+import T3.Game.BoardManager (BoardManager(..), isOpenLoc_, insertAtLoc_, getResult_)
+import T3.Game.Types (Win(..), Lose(..))
 import T3.Server.Types
-
-main :: Monad m => m ()
-main = return ()
 
 data Env = Env
   { _envResultsCb :: ResultsCb
@@ -20,8 +26,51 @@ data Env = Env
   , _envGameCbO :: GameCb
   }
 
-newtype Game a = Game (IO a)
-  deriving (Functor, Applicative, Monad, MonadIO)
+move' :: (MonadReader Env m, MonadIO m) => XO -> m Loc
+move' xo = do
+  cb <- asks (fst . (case xo of X -> _envGameCbX; O -> _envGameCbO))
+  liftIO $ readChan cb
+
+forfeit' :: Monad m => Win XO -> Lose XO -> m ()
+forfeit' = undefined
+
+end' :: Monad m => Win XO -> Lose XO -> m ()
+end' = undefined
+
+tie' :: Monad m => m ()
+tie' = undefined
+
+isOpenLoc' :: MonadState Board m => Loc -> m Bool
+isOpenLoc' loc = do
+  board <- get
+  return $ isOpenLoc_ loc board
+
+insertAtLoc' :: MonadState Board m => Loc -> XO -> m ()
+insertAtLoc' loc xo = do
+  board <- get
+  put $ insertAtLoc_ loc xo board
+
+getResult' :: MonadState Board m => m Result
+getResult' = do
+  board <- get
+  return $ getResult_ board
+
+newtype Game a = Game (StateT Board (ReaderT Env IO) a)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadReader Env, MonadState Board)
 
 run :: MonadIO m => Game a -> Env -> m a
-run (Game m) env = liftIO $ m
+run (Game m) env = liftIO $ runReaderT (evalStateT m emptyBoard) env
+
+instance Play Game where
+  play = play'
+
+instance Control Game where
+  move = move'
+  forfeit = forfeit'
+  end = end'
+  tie = tie'
+
+instance BoardManager Game where
+  isOpenLoc = isOpenLoc'
+  insertAtLoc = insertAtLoc'
+  getResult = getResult'
